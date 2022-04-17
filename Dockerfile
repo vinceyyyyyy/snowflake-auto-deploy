23af5c75c9bbdb5de42ce4f2e90593dd41b3472d
Vincent Yan <vincent.yan@blend360.com>
2022-04-17 12:15:33 -0400

    feat: use sql file instead of string

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,18 @@
-FROM python:3.8-slim
+FROM python:3.10-slim
 
 # Configure environments vars. Overriden by GitHub Actions
 ENV INPUT_SNOWFLAKE_ACCOUNT=
 ENV INPUT_SNOWFLAKE_USERNAME=
 ENV INPUT_SNOWFLAKE_PASSWORD=
 ENV INPUT_SNOWFLAKE_WAREHOUSE=
-ENV INPUT_QUERIES=
+ENV INPUT_QUERY_FILE=
 ENV APP_DIR=/app
 
 WORKDIR ${APP_DIR}
 
 # setup python environ
-COPY ./requirements.txt ${APP_DIR}
-RUN pip install -r ${APP_DIR}/requirements.txt
-
-# copy app files
-COPY . ./
+COPY . ${APP_DIR}
+RUN pip install --user pdm && pdm install
 
 # command to run in container start
-CMD python ${APP_DIR}/main.py
+CMD pdm run python ${APP_DIR}/main.py
--- main.py
+++ main.py
@@ -4,33 +4,35 @@ import json
 
 from dotenv import load_dotenv
 
-import utils
 from snowflake_connector import SnowflakeConnector
+import utils
+
 
 def main():
-    load_dotenv() # only on local run
+    load_dotenv()  # only on local run
     print(os.environ)
-    queries_list = os.environ['INPUT_QUERIES'].split(';')
+
+    query_file_name = os.environ["INPUT_QUERY_FILE"]
     sync = os.environ.get("INPUT_SYNC", False)
-    warehouse = os.environ['INPUT_SNOWFLAKE_WAREHOUSE']
-    snowflake_account = os.environ['INPUT_SNOWFLAKE_ACCOUNT']
-    snowflake_username = os.environ['INPUT_SNOWFLAKE_USERNAME']
-    snowflake_password = os.environ['INPUT_SNOWFLAKE_PASSWORD']
-    snowflake_role = os.environ.get('INPUT_SNOWFLAKE_ROLE', '')
-    
+    warehouse = os.environ["INPUT_SNOWFLAKE_WAREHOUSE"]
+    snowflake_account = os.environ["INPUT_SNOWFLAKE_ACCOUNT"]
+    snowflake_username = os.environ["INPUT_SNOWFLAKE_USERNAME"]
+    snowflake_password = os.environ["INPUT_SNOWFLAKE_PASSWORD"]
+    snowflake_role = os.environ.get("INPUT_SNOWFLAKE_ROLE", "")
+
+    with open(query_file_name, "r") as f:
+        sql = "".join(f.readlines())
+
     with SnowflakeConnector(snowflake_account, snowflake_username, snowflake_password) as con:
-        try:
-            if snowflake_role != '':
-                con.set_user_role(snowflake_role)
-        except:
-            pass
-        
+        if snowflake_role:
+            con.set_user_role(snowflake_role)
+
         con.set_db_warehouse(warehouse)
 
-        query_results = []
         # default, run all queries async
         if not sync:
-            for query in queries_list:
+            query_results = []
+            for query in sql.split(";"):
                 query_result = con.query(query)
                 query_results.append(query_result)
                 print("### Running query ###")
@@ -40,15 +42,15 @@ def main():
         # o/w, run them sync
         else:
             json_results = {}
-            for query in queries_list:
+            for query in sql.split(";"):
                 query_result = con.query(query)
                 print("### Running query ###")
                 print(f"[!] Query id - {query_result.query_id}")
                 print(f"[!] Running query ### - {query}")
                 json_results[query_result.query_id] = query_result.fetch_results_sync()
 
-    utils.set_github_action_output('queries_results', json.dumps(json_results))
+    utils.set_github_action_output("queries_results", json.dumps(json_results))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
