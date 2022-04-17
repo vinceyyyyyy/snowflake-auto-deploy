8b759f75efdb2329c88faca80710d6a80867b8f5
Vincent Yan <vincent.yan@blend360.com>
2022-04-17 12:15:18 -0400

    refactor: formatting and refactor

--- snowflake_connector.py
+++ snowflake_connector.py
@@ -4,6 +4,7 @@ import snowflake.connector
 
 from snowflake.connector.constants import QueryStatus
 
+
 class SnowflakeConnector:
     def __init__(self, account_name: str, username: str, password: str):
         self.account_name = account_name
@@ -12,9 +13,8 @@ class SnowflakeConnector:
 
     def __enter__(self):
         self.con = snowflake.connector.connect(
-        user=self.username,
-        password=self.password,
-        account=self.account_name)
+            user=self.username, password=self.password, account=self.account_name
+        )
 
         return self
 
@@ -22,11 +22,11 @@ class SnowflakeConnector:
         self.con.close()
 
     def set_db_warehouse(self, warehouse: str):
-        results = self.query(f'USE WAREHOUSE {warehouse}')
+        results = self.query(f"USE WAREHOUSE {warehouse}")
         return asyncio.run(results.fetch_results())
-    
+
     def set_user_role(self, role: str):
-        results = self.query(f'USE ROLE {role}')
+        results = self.query(f"USE ROLE {role}")
         return asyncio.run(results.fetch_results())
 
     def query(self, query_str: str):
@@ -34,6 +34,7 @@ class SnowflakeConnector:
         cursor.execute_async(query_str)
 
         return QueryResult(self.con, cursor.sfqid)
+
 
 class QueryResult:
     def __init__(self, con: SnowflakeConnector, query_id: str):
@@ -43,16 +44,18 @@ class QueryResult:
 
     def is_query_done_successfully(self):
         return self.con.get_query_status(self.query_id) == QueryStatus.SUCCESS
-    
+
     def is_query_running(self):
-        return (self.con.get_query_status(self.query_id) == QueryStatus.RUNNING or
-                self.con.get_query_status(self.query_id) == QueryStatus.NO_DATA)
+        return self.con.get_query_status(self.query_id) in [
+            QueryStatus.RUNNING,
+            QueryStatus.NO_DATA,
+        ]
 
     def _fetch_results(self):
         """
         Raises ProgrammingError in case of SQL error,
         by get_results_from_sfqid
-        """        
+        """
         self.cursor.get_results_from_sfqid(self.query_id)
 
         row = self.cursor.fetchone()
